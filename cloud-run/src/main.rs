a034d78e4e881b2c5442a7eb7f98b861fb131e27
terkwood <38859656+Terkwood@users.noreply.github.com>
2020-06-12 11:05:42 -0400

    trims

--- cloud-run/src/main.rs
+++ cloud-run/src/main.rs
@@ -1,13 +1,9 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 extern crate chrono;
 extern crate serde;
-extern crate serde_json;
-#[macro_use]
 extern crate serde_derive;
+extern crate serde_json;
 
 pub mod cors;
-pub mod models;
 pub mod routes;
 fn main() {
     todo!("oauth")
