0bf1dcfebb0f15d7b32af8ce1fed327dbc875b81
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-06-12 06:50:01 -0400

    Stub no auth (#5)
    * Add oauth2 dep
    
    * Stub out nothing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+// SPDX-License-Identifier: MIT
+extern crate log;
+extern crate tokio;
+extern crate warp;
+
+use log::info;
+use std::env;
+use warp::Filter;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    info!("{}", VERSION);
+
+    // GET /auth => 200 OK with body "NO AUTH YET"
+    let auth = warp::path!("auth").map(|| format!("NO AUTH YET"));
+
+    warp::serve(auth).run(([0, 0, 0, 0], 3030)).await;
 }
